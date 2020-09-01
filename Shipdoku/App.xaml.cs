a137ac3cdf2144fda5447b41e92592fe3df3a01c
Tim <tscheiber@online.gibz.ch>
2020-09-01 14:18:29 +0200

    Prism initialized

--- Shipdoku/App.xaml.cs
+++ Shipdoku/App.xaml.cs
@@ -5,13 +5,27 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+using Shipdoku.Views;
 
 namespace Shipdoku
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // ToDo: register needed services here
+            // containerRegistry.Register<ICustomerStore, DbCustomerStore>();
+        }
+
+        protected override Window CreateShell()
+        {
+            var w = Container.Resolve<Shell>();
+            return w;
+        }
     }
 }
