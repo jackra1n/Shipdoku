3f360b2bbe21872eb2856b9d73f01fcb15661422
Tim <tscheiber@online.gibz.ch>
2020-09-11 16:00:53 +0200

    Rename Project and Namespace

--- Shipdoku/App.xaml.cs
+++ Shipdoku/App.xaml.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 using System.Windows;
 using Prism.Ioc;
 using Prism.Unity;
-using ShipdokuGUI.Views;
+using Shipdoku.Views;
 
-namespace ShipdokuGUI
+namespace Shipdoku
 {
     /// <summary>
     /// Interaction logic for App.xaml
