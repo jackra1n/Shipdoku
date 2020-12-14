17d50c22dcee9603004dc410f72dbd5ce073624e
Tim <timscheiber87@gmail.com>
2020-12-14 19:04:58 +0100

    commented

--- Shipdoku/Interfaces/IShipdokuGenerator.cs
+++ Shipdoku/Interfaces/IShipdokuGenerator.cs
@@ -7,6 +7,10 @@ namespace Shipdoku.Interfaces
 {
     public interface IShipdokuGenerator
     {
+        /// <summary>
+        /// Generates a new Playingfield with all the Vertical and Horizontal Ship-Counts
+        /// </summary>
+        /// <returns>A new ShipdokuModel with the generated Playingfield</returns>
         ShipdokuModel GenerateShipdokuModel();
     }
 }
